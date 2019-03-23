0c9a59e0f3b3f0993bd8fb7c7919126e844cd789
cryptozeny <cryptozeny@gmail.com>
2019-03-23 20:04:01 +0900

    remove: sugar

--- max_money.py
+++ max_money.py
@@ -30,7 +30,7 @@ while current_reward > 1: # bigger than one satoshi
 
 # print total
 print "Total BTC to ever be created: %.2f" % total, "Satoshis"
-print "Total BTC to ever be created: %.0f" % round(total / 10**8), "BTC"
+print "Total BTC to ever be created: %.0f" % round(total / 10**8), "BTC (rounded)"
 
 
 # output example - BTC
@@ -73,41 +73,4 @@ Count	Supply			Reward
 33	2099999999755527.75	0.582076609134674072265625
 Total BTC to ever be created: 2099999999755527.75 Satoshis
 Total BTC to ever be created: 21000000 BTC
-"""
-
-
-#########
-### SUGAR
-#########
-
-# Original block reward for miners was 50 BTC
-start_block_reward = 50
-# 210000 is around every 4 years with a 10 minute block interval
-reward_interval = 210000*120
-
-def max_money_SUGAR():
-    # 50 BTC = 50 0000 0000 Satoshis
-    current_reward = 50 * 10**8
-    total = 0
-    while current_reward > 0:
-        total += reward_interval * current_reward
-        current_reward /= 2
-    return total
-
-print "\nSUGAR"
-print "Total SUGAR to ever be created:", max_money_SUGAR(), "Satoshis"
-print "Total SUGAR to ever be created:", max_money_SUGAR() / 10**8, "SUGAR"
-
-
-# output
-"""
-$ ./max_money.py 
-
-BTC
-Total BTC to ever be created: 2099999997690000 Satoshis
-Total BTC to ever be created: 20999999 BTC
-
-SUGAR
-Total SUGAR to ever be created: 251999999722800000 Satoshis
-Total SUGAR to ever be created: 2519999997 SUGAR
 """
