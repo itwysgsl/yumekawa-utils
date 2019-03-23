ee718dd70babb6a3a6fa17eaf7a1704524bd470e
cryptozeny <cryptozeny@gmail.com>
2019-03-23 16:04:58 +0900

    fix: rename && update

--- emission-iamstenman.py
+++ emission-iamstenman.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
-GRAPH_STEPS = 12 * 60 * 24 * 30  # 1 month
-GRAPH_YEARS = 100
+# setup
+OUT_FILE='emission-iamstenman'
+
+# GRAPH_STEPS = 12 * 60 * 24 * 30  # 1 month
+GRAPH_STEPS = 12 * 60 * 24 * 365  # 1 year
+GRAPH_YEARS = 4
 
 BLOCKS_PER_MINUTE = 12  # 5 secconds for block
 BLOCKS_PER_YEAR = BLOCKS_PER_MINUTE * 525960  # 525960 minutes in 1 year
-REWARD_PER_BLOCK = 12
+REWARD_PER_BLOCK = 50
 HALVING_YEARS = 2
-MAX_HALVINGS = 4
+MAX_HALVINGS = 0
 
 def supply(height=0):
 	reward = REWARD_PER_BLOCK
@@ -31,5 +35,11 @@ def supply(height=0):
 data = [supply(i) for i in range(0, BLOCKS_PER_YEAR * GRAPH_YEARS, GRAPH_STEPS)]
 csv = 'Height, Reward, Supply, Halvings, Year\n' + ''.join(data)
 
-with open('emission.csv', 'w') as file:
+with open('%s.csv' % OUT_FILE, 'w') as file:
 	file.write(csv)
+
+# cat file on console
+import os
+command = os.popen('cat %s.csv' % OUT_FILE)
+print(command.read())
+print(command.close())
